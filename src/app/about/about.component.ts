d195e2b533184a46438ff841e51140a0eb14731f
shashiweb <shashiweb64@gmail.com>
2024-12-18 16:04:18 +0530

    change

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
