286ee93a3b2e52607adeea6adc7d497d3ec3dcf8
shashiweb <shashiweb64@gmail.com>
2024-12-19 15:40:40 +0530

    change

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-about',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AboutComponent],
+  imports: [RouterOutlet,AboutComponent,ContactusComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
